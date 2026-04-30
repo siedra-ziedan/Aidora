2b78dcd9846e2d1421a1875e9e28f36db96b98cc
unknown <seidra>
2026-05-01 01:54:36 +0200

    Fix CORS issue for Flutter web integration  1. Add corsheaders to INSTALLED_APPS 2. Add CorsMiddleware at top of MIDDLEWARE 3. Enable CORS_ALLOW_ALL_ORIGINS for local development

--- Aidora/settings.py
+++ Aidora/settings.py
@@ -44,10 +44,12 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
     'django.contrib.postgres',
+    'corsheaders',
 ]
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,8 +57,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'Aidora.urls'
 
 TEMPLATES = [
